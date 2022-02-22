32ba7ff323ad62ab3f2680c484b0a1f5bf2d403b
Alex-Stupak <stupakalexx@gmail.com>
2022-02-22 22:19:53 +0200

    nex loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,21 +1,12 @@
-import { BallTriangle } from 'react-loader-spinner';
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import s from "./Loader.module.css";
+import { Oval } from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import s from './Loader.module.css';
 
-
-function Loader() {
-    return (
-        <div className={s.loader}>
-            <BallTriangle 
-                heigth="100"
-                width="100"
-                color='#3f51b5'
-                ariaLabel='loading'
-                className={s.loader}
-            />
-        </div>
-        
-    )
-}
-
+const Loader = () => {
+  return (
+    <div className={s.LoaderContainer}>
+      <Oval arialLabel="loading-indicator" />
+    </div>
+  );
+};
 export default Loader;
